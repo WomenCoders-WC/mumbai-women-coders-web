638a5c675a2e1c94581167d487f8ebe37a6cec2d
Deadpool2000 <d2kyt@protonmail.com>
2026-07-11 12:19:25 +0530

    Added new event

--- src/data/home-upcoming-events.ts
+++ src/data/home-upcoming-events.ts
@@ -15,5 +15,19 @@ export interface UpcomingEvent {
 }
 
 export const upcomingEvents: UpcomingEvent[] = [
-
+    {
+        id: "1",
+        image: "/assets/images/events/mwc-25-7.jpg",
+        imageAlt: "One Prompt Is Not Enough - MWC",
+        badge: "Mumbai Chapter",
+        dateDay: "25",
+        dateMonth: "JULY",
+        title: "Mumbai Women Coders Presents: One Prompt Is Not Enough",
+        locationShort: "Mumbai",
+        locationFull: "ATLAS SkillTech University, Mumbai",
+        eventType: "Community Event",
+        eventFormat: "In-Person",
+        eventIcon: "fa-laptop",
+        registrationLink: "https://luma.com/rpjnqwjz"
+    },
 ];
